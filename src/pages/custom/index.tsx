bba979913e15d161c4a4f23948770ef9b65a8882
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-21 01:48:48 +0800

    feat: custom

---
+++ src/pages/custom/index.tsx
@@ -0,0 +1,14 @@
+function CustomPage() {
+  return (
+    <>
+      <p className='text-xs sm:text-base lg:text-lg'>
+        Hello World!
+      </p>
+      <br/>
+
+      <p className='mt-6'>Test Spacing</p>
+    </>
+  )
+}
+
+export default CustomPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px', // @media (max-width: 768px)
+      lg: '976px', // @media (max-width: 976px)
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
     // 扩展配置项，不覆盖tailwind原有的配置项
     extend: {
       colors: {
@@ -17,5 +31,6 @@ module.exports = {
       }
     },
   },
+  // help to create style-sheets with javascript
   plugins: [],
 }
