8c6ca335069b799d94a32b0917b2c7e62c566a36
lesenelir <miaomiaobabyzy@gmail.com>
2023-04-21 22:22:58 +0800

    chore: udpate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 function Home() {
   return (
     <div className='min-h-full flex justify-center items-center'>
-       <ul>
+       <ul className='list-disc list-outside'>
          <li><Link href={'/color'}>Color Page</Link></li>
          <li><Link href={'/custom'}>Custom Page</Link></li>
          <li><Link href={'/typography'}>Typography Page</Link></li>
